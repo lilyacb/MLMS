94f58c4a892f193e18bffeb062623ef5a301c916
lilyacb <lily-clough@utulsa.edu>
2021-06-10 19:25:47 -0500

    doc

---
+++ R/MLMSfunctions.R
@@ -0,0 +1,13 @@
+#### Functions for processing .dxf MS data files
+
+#' read_summ: read and print a summary of mass spec data from a .dxf file
+#' @param filename character string of the name of the .dxf file of data
+#' @return summary table of file contents
+#' @example
+#' Usage example
+#' @export
+read_summ<-function(filename){
+  msdat<-iso_read_continuous_flow(filename)
+  summ<-summary(msdat)
+  print(summ)
+}
